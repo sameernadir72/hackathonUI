c5e7313ea4b54b28c7fa4962c168dffd3081b4be
Muhammad Masood <yoursourcecode@gmail.com>
2023-06-26 10:57:18 -0700

    product card

---
+++ components/section/ProductCard.tsx
@@ -0,0 +1,14 @@
+import Image, { StaticImageData } from "next/image";
+
+const ProductCard = (props:{title:string, category:string, price:number, image:StaticImageData}) => {
+  return (
+    <div className="">
+        <Image src={props.image} alt="product image"></Image>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">{props.title}</p>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">{props.category}</p>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">{props.price}</p>
+    </div>
+  )
+}
+
+export default ProductCard; 
