f8037134c1401d9698e21abc57e84099e79e5562
Muhammad Masood <yoursourcecode@gmail.com>
2023-06-30 08:07:00 -0700

    commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,13 +1,31 @@
 import { products } from "@/app/utils/mock";
 import { ProductView } from "../../../../components/section/ProductView";
 import { Product } from "@/app/utils/types";
+import ProductCard from "../../../../components/section/ProductCard";
 
+const filterProducts = (id:number) => {
+  return (products.filter((product) => (
+      product.id === id
+  )))
+}
 export default function Page({ params }: { params: { id: number} }) {
     const product:Product = products[params.id-1];
+    const filteredProducts = filterProducts(params.id);
     return (
 
       <div>
         <ProductView {...product} />
+
+    <div >
+      <h1 className="font-extrabold"></h1>
+      {
+        filteredProducts.map((product,index) => (
+            <ProductCard key={index} id={product.id} title={product.name} category={product.category}
+            price={product.price} image={product.image} ></ProductCard>
+        ))
+      }
+
+      </div>
       </div>
   )
 }
